e94c164ac1567a40cbc1af1b8739d34bcf5b49e9
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2016-05-12 22:10:16 +0530

    CompanyCustomerUpdate2

--- UserModule/src/main/java/edu/uom/enex/server/controller/CompanyCustomerController.java
+++ UserModule/src/main/java/edu/uom/enex/server/controller/CompanyCustomerController.java
@@ -1,28 +1,15 @@
 package edu.uom.enex.server.controller;
-<<<<<<< HEAD
 
-=======
->>>>>>> 85ff9d12bdb52f1138c51f8e3ba05d1f36c822f7
 import edu.uom.enex.server.entity.CompanyCustomer;
 import edu.uom.enex.server.service.CustomerDAOService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
  * Created by Himashi Nethinika on 5/12/2016.
-=======
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-/**
- * Created by Kavindya on 5/12/2016.
->>>>>>> 85ff9d12bdb52f1138c51f8e3ba05d1f36c822f7
  */
 
 @Controller
@@ -32,20 +19,11 @@ public class CompanyCustomerController {
     @Autowired
     private CustomerDAOService customerDAOService;
 
-<<<<<<< HEAD
     @RequestMapping(value = "ob", method = RequestMethod.GET, headers = "Accept=application/json")
     @ResponseBody
     public CompanyCustomer ob() {
         return new CompanyCustomer();
     }
-=======
-    // must edit the constructor..
-//    @RequestMapping(value = "ob", method = RequestMethod.GET, headers = "Accept=application/json")
-//    @ResponseBody
-//    public CompanyCustomer ob() {
-//        return new CompanyCustomer();
-//    }
->>>>>>> 85ff9d12bdb52f1138c51f8e3ba05d1f36c822f7
 
     /**
      * save Customer
@@ -57,11 +35,7 @@ public class CompanyCustomerController {
     @ResponseBody
     public ResponseMessage addCustomer(@RequestBody CompanyCustomer customer) {
         ResponseMessage responseMessage;
-<<<<<<< HEAD
         String res = customerDAOService.saveCustomer(customer,"CC-");
-=======
-        String res = customerDAOService.saveCustomer(customer, "IC");
->>>>>>> 85ff9d12bdb52f1138c51f8e3ba05d1f36c822f7
         if (res != null) {
             responseMessage = ResponseMessage.SUCCESS;
             responseMessage.setData(res);
@@ -93,7 +67,6 @@ public class CompanyCustomerController {
         }
         return responseMessage;
     }
-<<<<<<< HEAD
 
     /**
      * get all Customers
@@ -131,6 +104,3 @@ public class CompanyCustomerController {
 
 
 }
-=======
-}
->>>>>>> 85ff9d12bdb52f1138c51f8e3ba05d1f36c822f7
